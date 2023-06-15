7a895ebbd2aa7e80f22f9c76fdcb1f867c1240f3
Ella Hoeppner <ellahoeppner@gmail.com>
2023-06-14 21:53:39 -0400

    adding basic control scheme, demonstrating in float unistack demo

---
+++ src/linevo/controllers/basic.cljs
@@ -0,0 +1,82 @@
+(ns linevo.controllers.basic
+  (:require [linevo.rand :refer [rand-fn-rand-int]]
+            [linevo.gp :refer [breed]]
+            [sprog.util :as u]))
+
+(defn create-controller! [op-generator
+                          & [{:keys [rand-fn
+                                     scratch-min-length
+                                     scratch-max-length
+                                     breed-min-length
+                                     breed-max-length
+                                     initial-population-size
+                                     initial-population]
+                              :or {rand-fn rand
+                                   scratch-min-length 5
+                                   scratch-max-length 15
+                                   breed-min-length 1
+                                   breed-max-length 40
+                                   initial-population-size 1}}]]
+  (let [scratch-program #(vec (repeatedly
+                               (rand-fn-rand-int
+                                rand-fn
+                                scratch-min-length
+                                (inc scratch-max-length))
+                               op-generator))
+        population-atom (atom (vec (if initial-population
+                                     initial-population
+                                     (repeatedly initial-population-size
+                                                 scratch-program))))
+        deletions-atom (atom ())
+        index-atom (atom 0)
+        breed-program (fn []
+                        (some #(when (<= breed-min-length
+                                         (count %)
+                                         breed-max-length)
+                                 %)
+                              (repeatedly #(breed op-generator
+                                                  @population-atom))))
+        rectify-index! (fn []
+                         (swap! index-atom
+                                #(-> %
+                                     (max 0)
+                                     (min (dec (count @population-atom))))))
+        move-index! (fn [movement]
+                      (swap! index-atom (partial + movement))
+                      (rectify-index!))]
+    {:scratch (fn []
+                (reset! index-atom (count @population-atom))
+                (swap! population-atom #(conj % (scratch-program)))
+                (last @population-atom))
+     :breed (fn []
+              (reset! index-atom (count @population-atom))
+              (swap! population-atom
+                     #(conj % (breed-program)))
+              (last @population-atom))
+     :delete (fn [& [population-index]]
+               (when (> (count @population-atom) 1)
+                 (let [index (or population-index @index-atom)]
+                   (swap! deletions-atom
+                          #(conj % (nth @population-atom index)))
+                   (swap! population-atom
+                          #(vec (concat (take index %)
+                                        (drop (inc index) %))))
+                   (rectify-index!)))
+               (@population-atom @index-atom))
+     :undelete (fn []
+                 (when (pos? (count @deletions-atom))
+                   (reset! index-atom (count @population-atom))
+                   (swap! population-atom #(conj % (first @deletions-atom)))
+                   (swap! deletions-atom rest))
+                 (@population-atom @index-atom))
+     :move (fn [movement]
+             (move-index! movement)
+             (@population-atom @index-atom))
+     :set-index (fn [index]
+                  (reset! index-atom index)
+                  (rectify-index!)
+                  (@population-atom @index-atom))
+     :get-program (fn [& [population-index]]
+                    (let [index (or population-index @index-atom)]
+                      (@population-atom index)))
+     :get-population (fn [] @population-atom)}))
--- src/linevo/demos/float_unistack_demo.cljs
+++ src/linevo/demos/float_unistack_demo.cljs
@@ -1,23 +1,24 @@
 (ns linevo.demos.float-unistack-demo
   (:require [sprog.util :as u]
             [sprog.webgl.core
-             :refer [start-sprog!]
+             :refer [start-sprog!
+                     sprog-context
+                     sprog-state]
              :refer-macros [with-context]]
             [sprog.webgl.shaders :refer [run-purefrag-shader!]]
-            [sprog.webgl.textures :refer [create-tex]]
+            [sprog.webgl.textures :refer [create-tex
+                                          set-tex-data!]]
             [sprog.dom.canvas :refer [maximize-gl-canvas
                                       canvas-resolution]]
             [linevo.engines.float-unistack :refer [eval-program
-                                                   get-op-generator]]))
+                                                   get-op-generator]]
+            [linevo.controllers.basic :refer [create-controller!]]
+            [sprog.input.keyboard :refer [add-key-callback
+                                          add-left-right-key-callback]]))
 
-(let [op-generator (get-op-generator)
-      random-program (vec (repeatedly 15 op-generator))]
-  (defn random-fn [x y]
-    (take 3
-          (concat (map u/sigmoid
-                       (eval-program [x y]
-                                     random-program))
-                  (repeat 0.5)))))
+(def pasma-resolution 50)
+
+(def op-generator (get-op-generator))
 
 (def frag-shader
   '{:version "300 es"
@@ -31,7 +32,34 @@
     ((= frag-color
         (vec4 (.rgb (texture tex (/ gl_FragCoord.xy size))) 1)))})
 
-(defn init-sprog! [gl]
+(defn get-program-data [program]
+  (let [program-fn (fn [x y]
+                     (take 3
+                           (concat (map u/sigmoid
+                                        (eval-program [x y]
+                                                      program))
+                                   (repeat 0.5))))]
+    (js/Uint8Array.
+     (mapcat (fn [coords]
+               (conj (mapv #(min 255 (* % 255))
+                           (apply program-fn coords))
+                     0))
+             (for [x (range pasma-resolution)
+                   y (range pasma-resolution)]
+               (mapv #(-> %
+                          (/ (dec pasma-resolution))
+                          (* 2)
+                          (- 1))
+                     [x y]))))))
+
+(defn display-program-pasma! [program]
+  (set-tex-data! (sprog-context)
+                 (:tex (sprog-state))
+                 :f8
+                 pasma-resolution
+                 (get-program-data program)))
+
+(defn update-sprog! [{:keys [gl tex] :as state}]
   (with-context gl
     (maximize-gl-canvas {:square? true})
     (let [resolution (canvas-resolution)]
@@ -39,24 +67,27 @@
        frag-shader
        resolution
        {"size" resolution
-        "tex" (let [pasma-size 100]
-                (create-tex
-                 :f8
-                 pasma-size
-                 {:filter-mode :nearest
-                  :data
-                  (js/Uint8Array.
-                   (mapcat (fn [coords]
-                             (conj (mapv #(min 255 (* % 255))
-                                         (apply random-fn coords))
-                                   0))
-                           (for [x (range pasma-size)
-                                 y (range pasma-size)]
-                             (mapv #(-> %
-                                        (/ (dec pasma-size))
-                                        (* 2)
-                                        (- 1))
-                                   [x y]))))}))}))))
+        "tex" tex}))
+    state))
+
+(defn init-sprog! [gl]
+  (with-context gl
+    (let [{:keys [move
+                  get-program]
+           :as controller}
+          (create-controller! op-generator)
+          display! #(display-program-pasma! (get-program))]
+      (doseq [[key-name fn-name]
+              [["s" :scratch]
+               ["b" :breed]
+               ["x" :delete]
+               ["z" :undelete]]]
+        (add-key-callback key-name #(do ((fn-name controller)) (display!))))
+      (add-left-right-key-callback #(do (move %) (display!)))
+      {:tex (create-tex :f8
+                        pasma-resolution
+                        {:filter-mode :nearest
+                         :data (get-program-data (get-program))})})))
 
 (defn init []
   (js/window.addEventListener
@@ -64,4 +95,4 @@
    (fn [_]
      (start-sprog!
       init-sprog!
-      nil))))
+      update-sprog!))))
